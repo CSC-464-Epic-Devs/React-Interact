40c58c5720d769993cd178a8f73e7104ded1f2d2
Adeshkumar Naik <adeshkumar.naik@mines.sdsmt.edu>
2019-11-03 17:07:18 -0700

    Pushed our first change with buttons
    Used Material for button need to add dependency

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,27 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
+import Button from "@material-ui/core/Button";
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="Home Page" />
+    <h1>React Interact</h1>
+    <h2>Regular Devs</h2>
+
+    <Button
+      variant="contained"
+      color="primary"
+      style={{ marginRight: "100px" }}
+    >
+      Learn ReactJS
+    </Button>
+
+    <Button variant="contained" color="primary">
+      All Tutorials
+    </Button>
+
+    <br />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
