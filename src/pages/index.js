53c081c5a825488f8a40924c492936de3fa0f118
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2019-11-05 22:13:50 -0700

    added banners to home page
    added logo png too, should probably be replaced in the future, just a filler for now. Webpage still needs work.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,77 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
-
+import Image from "../components/image";
 import Button from "@material-ui/core/Button";
+
+const bannerHeight = "250px";
+
+const TopBanner = () => (
+  <div
+    style={{
+      background: "black",
+      color: "white",
+      height: bannerHeight
+    }}
+  >
+    <div style={{ width: bannerHeight, float: "right" }}>
+      <Image />
+    </div>
+    <h1>React Interact</h1>
+    <h2>Regular Devs</h2>
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginRight: "10%", background: "teal" }}
+      >
+        Learn ReactJS
+      </Button>
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ background: "teal" }}
+      >
+        All Tutorials
+      </Button>
+    </div>
+  </div>
+);
+
+const OurGoalBanner = () => (
+  <div
+    style={{
+      textAlign: "center",
+      backgroundImage: "linear-gradient(teal, blue)",
+      height: bannerHeight,
+      padding: "10% 0"
+    }}
+  >
+    <h2>Our Goal</h2>
+  </div>
+);
+
+const InfoBanner = () => (
+  <div
+    style={{
+      textAlign: "center",
+      backgroundImage: "linear-gradient(teal, blue)",
+      height: bannerHeight,
+      padding: "10% 0"
+    }}
+  >
+    <h2>Our Goal</h2>
+  </div>
+);
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home Page" />
-    <h1>React Interact</h1>
-    <h2>Regular Devs</h2>
-
-    <Button
-      variant="contained"
-      color="primary"
-      style={{ marginRight: "100px" }}
-    >
-      Learn ReactJS
-    </Button>
-
-    <Button variant="contained" color="primary">
-      All Tutorials
-    </Button>
-
+    <TopBanner />
+    <OurGoalBanner />
+    <InfoBanner />
     <br />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
