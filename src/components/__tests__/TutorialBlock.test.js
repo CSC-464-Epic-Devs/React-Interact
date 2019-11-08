dbba5af13a46d427cfc337329f2bac90c13f19b8
Lewis,Youki <youki.lewis@mines.sdsmt.edu>
2019-11-08 10:02:26 -0700

    Test Environment setup, minor eslint and prettier config

--- src/components/__tests__/TutorialBlock.test.js
+++ src/components/__tests__/TutorialBlock.test.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import TutorialBlock from '../TutorialBlock';
+
+// Check if component renders
+it('renders', () => {
+    render(<TutorialBlock />);
+});
 
