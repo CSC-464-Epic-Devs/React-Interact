02a1c81e441591460859770ba20bf84419d570cf
Adeshkumar Naik <adeshkumar.naik@mines.sdsmt.edu>
2019-11-08 15:31:57 -0700

    Our Goal is Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,18 +44,30 @@ const OurGoalBanner = () => (
     }}
   >
     <h2>Our Goal</h2>
+    <p>
+      Normally when programmers want to learn react or any other technology, the
+      programmer needs to resort to documentation provided by the creator or
+      other external educational tools.However the documentation may not serve
+      as the best tool for learning as it requires learner to jump multiple
+      places to get the required information to understand the concepts fully.
+      Also other educational are either free or have minimal information or are
+      expensive. React interact would allow anyone to hope on and learn React on
+      their own time while providing guidance to reduce the difficulty and time
+      it takes to learn new technology.
+    </p>
   </ContentBanner>
 );
 
 const InfoBanner = () => (
   <ContentBanner
     style={{
-      textAlign: "center",
+      textAlign: "justify",
       backgroundImage: "linear-gradient(teal, blue)",
       padding: "10% 0"
     }}
   >
-    <h2>Our Goal</h2>
+    <h2>30</h2>
+    <h2>60</h2>
   </ContentBanner>
 );
 
