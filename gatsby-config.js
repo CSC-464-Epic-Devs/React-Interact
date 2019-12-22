09dc0025c885186de3f7de87300050a65d76e9b3
Dustin Robert Kenneth Cook <dustin@localhost.localdomain>
2019-12-22 02:38:06 -0700

    changed site title and meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: 'React-Interact/',
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'React-Interact',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+      'Tutorial website devoted to encouraging aspiring developers to use the react framework',
     author: 'Regular Developers'
   },
   plugins: [
