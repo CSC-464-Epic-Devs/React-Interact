9419caeb73fb21f65282c4fe890910ffe03e6be6
Dustin Robert Kenneth Cook <dustin@localhost.localdomain>
2019-12-22 19:59:45 -0700

    sitemap xml is now available for search engines to crawl at /sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,62 @@
 module.exports = {
-  pathPrefix: 'React-Interact/',
+  pathPrefix: "React-Interact/",
   siteMetadata: {
-    title: 'React-Interact',
+    title: "React-Interact",
     description:
-      'Tutorial website devoted to encouraging aspiring developers to use the react framework',
-    author: 'Regular Developers'
+      "Tutorial website devoted to encouraging aspiring developers to use the react framework",
+    author: "Regular Developers"
   },
   plugins: [
-    'gatsby-plugin-material-ui',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-material-ui",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
+        name: "images",
         path: `${__dirname}/src/images`
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'pages',
+        name: "pages",
         path: `${__dirname}/src/pages/`
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'pages',
+        name: "pages",
         path: `${__dirname}/src/MDX/`
       }
     },
-    'gatsby-plugin-mdx',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#000000',
-        theme_color: '#000000',
-        display: 'minimal-ui',
-        icon: 'src/images/ReactLogo.png' // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#000000",
+        display: "minimal-ui",
+        icon: "src/images/ReactLogo.png" // This path is relative to the root of the site.
       }
     },
     {
-      resolve: 'gatsby-background-image-es5',
+      resolve: "gatsby-background-image-es5",
       options: {
         // add your own characters to escape, replacing the default ':/'
-        specialChars: '/:'
+        specialChars: "/:"
       }
     },
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-catch-links'
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-catch-links",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`
   ]
 };
